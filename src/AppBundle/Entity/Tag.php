53665d9bb79ecdb6178acab286b9cce662f8d595
Zander Baldwin <zander.baldwin@basekit.com>
2016-01-28 17:48:26 +0000

    Add AppBundle

---
+++ src/AppBundle/Entity/Tag.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tags")
+ */
+class Tag
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var integer
+     */
+    protected $id;
+}
